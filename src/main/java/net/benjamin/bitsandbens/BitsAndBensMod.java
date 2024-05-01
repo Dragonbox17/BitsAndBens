884196adc985d16b6b2d6b1926551d34f5f6fa4a
Dragonbox17 <BenjaminMoroniJohnson@gmail.com>
2024-04-30 21:30:29 -0600

    Added Raw Platinum and Platinum Ingot

--- src/main/java/net/benjamin/bitsandbens/BitsAndBensMod.java
+++ src/main/java/net/benjamin/bitsandbens/BitsAndBensMod.java
@@ -1,6 +1,8 @@
 package net.benjamin.bitsandbens;
 
 import com.mojang.logging.LogUtils;
+import net.benjamin.bitsandbens.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +28,8 @@ public class BitsAndBensMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -44,7 +48,10 @@ public class BitsAndBensMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.RAW_PLATINUM);
+            event.accept(ModItems.PLATINUM_INGOT);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
